d387280ae31a8dea20f5db5bd5a65b583bec4b1c
xMoha <40671043+xMoha@users.noreply.github.com>
2018-09-11 13:08:05 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,8 +2,33 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const prefix = "-";
 
-
-
+client.on('message', message => {
+    
+        if (message.author.id === client.user.id) return;
+        if (message.guild) {
+       let embed = new Discord.RichEmbed()
+        let args = message.content.split(' ').slice(1).join(' ');
+    if(message.content.split(' ')[0] == prefix + 'bc') {
+        if (!args[1]) {
+    message.channel.send("**-bc <message>**");
+    return;
+    }
+            message.guild.members.forEach(m => {
+       if(!message.member.hasPermission('ADMINISTRATOR')) return;
+                var bc = new Discord.RichEmbed()
+                .addField('» السيرفر :', `${message.guild.name}`)
+                .addField('» المرسل : ', `${message.author.username}#${message.author.discriminator}`)
+                .addField(' » الرسالة : ', args)
+                .setColor('#ff0000')
+                // m.send(`[${m}]`);
+                m.send(`${m}`,{embed: bc});
+            });
+        }
+        } else {
+            return;
+        }
+    });
+    
 
 const adminprefix = "-";
 const devs = ['455108784579149835','452785037511753728'];
